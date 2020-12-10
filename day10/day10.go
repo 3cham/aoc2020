6073187211c62dbb2b03807c1d2a24fc9039d23d
3cham <thanhtung.nov@gmail.com>
2020-12-10 21:17:40 +0100

    add day10 part2

--- day10/day10.go
+++ day10/day10.go
@@ -11,6 +11,27 @@ func main() {
 	path := "/Users/tdang/go/src/github.com/3cham/aoc2020/day10/input.txt"
 	content := utils.ReadInput(path)
 	fmt.Println(findDist(content))
+	fmt.Println(findArrangementCount(content))
+}
+
+func findArrangementCount(content string) int {
+	lines := strings.Split(content, "\n")
+	numbers := utils.ConvertStringArrToIntArr(lines)
+	numbers = append(numbers, 0)
+
+	sort.Ints(numbers)
+	count := make([]int, len(numbers))
+	count[0] = 1
+	for i := 1; i < len(numbers); i++ {
+		for j := i - 1; j >= 0; j-- {
+			if numbers[i] - numbers[j] <= 3 {
+				count[i] += count[j]
+			} else {
+				break
+			}
+		}
+	}
+	return count[len(numbers) - 1]
 }
 
 func findDist(content string) int {
@@ -31,3 +52,4 @@ func findDist(content string) int {
 	}
 	return count1 * (count3 + 1)
 }
+
