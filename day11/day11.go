d4c3d8b3af68c994d9c925c27ec57bd1658b024e
3cham <thanhtung.nov@gmail.com>
2020-12-11 22:13:29 +0100

    add day11 part1, part2

--- day11/day11.go
+++ day11/day11.go
@@ -10,12 +10,123 @@ func main() {
 	path := "/Users/tdang/go/src/github.com/3cham/aoc2020/day11/input.txt"
 	content := utils.ReadInput(path)
 	fmt.Println(countOccupied(change(content)))
+	fmt.Println(countOccupied(change2(content)))
 }
 
 func countOccupied(s string) int {
 	return strings.Count(s, "#")
 }
 
+func move(content string, countNeighborFunc func([]string, int, int) int, threshold int) string {
+	lines := strings.Split(content, "\n")
+	newContent := ""
+	for i := 0; i < len(lines); i++ {
+		for j := 0; j < len(lines[i]); j++ {
+			x := countNeighborFunc(lines, i, j)
+			if x == 0 && lines[i][j] == 'L' {
+				newContent += "#"
+				continue
+			}
+			if x >= threshold && lines[i][j] == '#' {
+				newContent += "L"
+				continue
+			}
+			newContent += string(lines[i][j])
+		}
+		if i < len(lines) - 1 {
+			newContent += "\n"
+		}
+	}
+	return newContent
+}
+
+func countNeighbor(lines []string, i,j int) int {
+	neighbors := []struct {
+		x int
+		y int
+	}{
+		{-1,-1},
+		{-1,0},
+		{-1,1},
+		{0,-1},
+		{0,1},
+		{1,-1},
+		{1,0},
+		{1,1},
+	}
+	count := 0
+	for _, neighbor := range neighbors {
+		ii := i + neighbor.x
+		jj := j + neighbor.y
+
+		if ii >= 0 && ii < len(lines) && jj >= 0 && jj < len(lines[i]) {
+			if lines[ii][jj] == '#' {
+				count++
+			}
+		}
+	}
+	return count
+}
+
+func countNeighborInDirection(lines []string, i,j int) int {
+	neighbors := []struct {
+		x int
+		y int
+	}{
+		{-1,-1},
+		{-1,0},
+		{-1,1},
+		{0,-1},
+		{0,1},
+		{1,-1},
+		{1,0},
+		{1,1},
+	}
+	count := 0
+
+	for _, neighbor := range neighbors {
+		a,b := i,j
+
+		for {
+			ii,jj := a + neighbor.x, b + neighbor.y
+			if ii >= 0 && ii < len(lines) && jj >= 0 && jj < len(lines[i]) {
+				if lines[ii][jj] == '#' {
+					count++
+					break
+				}
+				if lines[ii][jj] == 'L' {
+					break
+				}
+			} else {
+				break
+			}
+			a, b = ii,jj
+		}
+	}
+	return count
+}
+
 func change(content string) string {
-	return ""
+	for {
+		newContent := move(content, countNeighbor, 4)
+		if newContent == content {
+			return newContent
+		} else {
+			content = newContent
+		}
+	}
+	return content
+}
+
+
+func change2(content string) string {
+	for {
+		newContent := move(content, countNeighborInDirection, 5)
+		if newContent == content {
+			return newContent
+		} else {
+			content = newContent
+		}
+	}
+	return content
 }
--- day11/day11_test.go
+++ day11/day11_test.go
@@ -24,3 +24,67 @@ func TestCountOccupied(t *testing.T) {
 		})
 	}
 }
+
+func TestCountNeighbors(t *testing.T) {
+	lines := []string {
+		"#.##.##.##",
+		"#######.##",
+		"#.#.#..#..",
+	}
+
+	testCases := []struct {
+		x, y, count int
+	}{
+		{0,0, 2},
+		{1,1,6},
+		{2,2,3},
+	}
+
+	for _, testCase := range testCases {
+		t.Run("countNeighbors() should return correct number of occupied seats", func(t *testing.T) {
+			got := countNeighbor(lines, testCase.x, testCase.y)
+			if got != testCase.count {
+				t.Fatalf("Incorrect number of occupied seats, got %d, expected %d", got, testCase.count)
+			}
+		})
+	}
+}
+
+
+func TestCountNeighborInDirection(t *testing.T) {
+	lines := []string {
+		"#.##..#.L.",
+		"######...L",
+		"#.#.#..#..",
+	}
+
+	testCases := []struct {
+		x, y, count int
+	}{
+		{1,9, 1},
+		{2,9,1},
+		{2,7,1},
+	}
+
+	for _, testCase := range testCases {
+		t.Run("countNeighborIndirection() should return correct number of occupied seats", func(t *testing.T) {
+			got := countNeighborInDirection(lines, testCase.x, testCase.y)
+			if got != testCase.count {
+				t.Fatalf("Incorrect number of occupied seats, got %d, expected %d", got, testCase.count)
+			}
+		})
+	}
+}
+func TestMove(t *testing.T) {
+	content := 	"#.##.##.##\n"+
+				"#######.##\n"+
+				"#.#.#..#.."
+	got := move(content, countNeighbor, 4)
+	expected := "#.LL.L#.##\n"+
+				"#LLLLLL.L#\n"+
+				"#.#.#..#.."
+
+	if got != expected {
+		t.Fatalf("Wrong move, got \n%s, expected \n%s", got, expected)
+	}
+}
